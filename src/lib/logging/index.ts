524cbe5d9061f7cab3e07af7e5e58de0de398147
Stefano Labianca <lstefano@proton.me>
2025-12-29 23:00:32 +0100

    implemented a basic logger that write everything inside a file

--- src/lib/logging/index.ts
+++ src/lib/logging/index.ts
@@ -2,6 +2,108 @@ import { Formatter } from "./formatter.js";
 import { LogDataType, type LoggingOptions, LogLevel, LogLevelType } from "./types.js";
 
 
+export class FileLogger {
+  protected options: LoggingOptions;
+  protected formatter!: Formatter;
+  protected file: Bun.FileSink;
+
+  constructor(path: string, options: LoggingOptions) {
+    this.options = options;
+    this.file = Bun.file(path).writer();
+  }
+
+  protected getLogLevel() {
+    if (!this.options.level) {
+      return LogLevel.DEBUG;
+    }
+
+    return this.options.level;
+  }
+
+  public setFormatter(formatter: Formatter) {
+    this.formatter = formatter;
+  }
+
+  protected createLogData(level: LogLevelType, msg: string): LogDataType {
+    return {
+      level, msg, prefix: this.options.prefix
+    };
+  }
+
+
+  public debug(msg: string) {
+    const level = this.getLogLevel();
+    if (level > LogLevel.DEBUG) return;
+
+    if (this.formatter) {
+      const data = this.createLogData("DEBUG", msg);
+      this.file.write(this.formatter.format(data));
+    } else {
+      this.file.write(msg);
+    }
+
+    this.file.write("\n")
+  }
+
+  public info(msg: string) {
+    const level = this.getLogLevel();
+    if (level > LogLevel.INFO) return;
+
+    if (this.formatter) {
+      const data = this.createLogData("INFO", msg);
+      this.file.write(this.formatter.format(data));
+    } else {
+      this.file.write(msg);
+    }
+
+    this.file.write("\n")
+  }
+
+  public warning(msg: string) {
+    const level = this.getLogLevel();
+    if (level > LogLevel.WARNING) return;
+
+    if (this.formatter) {
+      const data = this.createLogData("WARNING", msg);
+      this.file.write(this.formatter.format(data));
+    } else {
+      this.file.write(msg);
+    }
+
+    this.file.write("\n")
+  }
+
+  public error(msg: string) {
+    const level = this.getLogLevel();
+    if (level > LogLevel.ERROR) return;
+
+    if (this.formatter) {
+      const data = this.createLogData("ERROR", msg);
+      this.file.write(this.formatter.format(data));
+    } else {
+      this.file.write(msg);
+    }
+
+    this.file.write("\n")
+  }
+
+  public critical(msg: string) {
+    const level = this.getLogLevel();
+    if (level > LogLevel.CRITICAL) return;
+
+    if (this.formatter) {
+      const data = this.createLogData("CRITICAL", msg);
+      this.file.write(this.formatter.format(data));
+    } else {
+      this.file.write(msg);
+    }
+
+    this.file.write("\n")
+  }
+}
+
+
+
 export class ConsoleLogger {
   protected options: LoggingOptions;
   protected formatter!: Formatter;
