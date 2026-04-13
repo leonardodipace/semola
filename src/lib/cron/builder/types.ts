95401195002b36dbeb599bed1cd6c27b1a2ac1fc
Stefano Labianca <lstefano@proton.me>
2026-04-13 20:08:31 +0200

    created constants for weekdays and months

---
+++ src/lib/cron/builder/types.ts
@@ -0,0 +1,24 @@
+export const WeekDay = {
+  sun: 0,
+  mon: 1,
+  tue: 2,
+  wen: 3,
+  thu: 4,
+  fri: 5,
+  sat: 6,
+} as const;
+
+export const Month = {
+  jan: 1,
+  feb: 2,
+  mar: 3,
+  apr: 4,
+  may: 5,
+  jun: 6,
+  jul: 7,
+  aug: 8,
+  sep: 9,
+  oct: 10,
+  nov: 11,
+  dec: 12,
+} as const;
