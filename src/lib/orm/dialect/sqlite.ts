54332cc3392b237501bbcfba211c20b82f33f5a5
Leonardo <leonardodipace@gmail.com>
2026-05-03 02:06:27 -0400

    Use SELECT *

--- src/lib/orm/dialect/sqlite.ts
+++ src/lib/orm/dialect/sqlite.ts
@@ -1,15 +1,11 @@
 import type { Table } from "../table/types.js";
-import { quoteIdentifier } from "../utils.js";
 import type { Dialect } from "./types.js";
 
 export const createSqliteDialect = <T extends Table>(table: T): Dialect<T> => {
   return {
     name: "sqlite",
     findMany: async (sql: Bun.SQL) => {
-      const tableName = sql(table.sqlName);
-      const columns = Object.keys(table.columns).map(quoteIdentifier);
-
-      return await sql`SELECT ${columns} FROM ${tableName}`;
+      return await sql`SELECT * FROM ${sql(table.sqlName)}`;
     },
   };
 };
