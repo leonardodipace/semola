849647cfb608175a6375114e019fb37072ffca7b
Leonardo Dipace <leonardo@softdrive.co>
2026-05-05 19:17:35 +0000

    Update example to include date field

--- src/lib/orm/index.ts
+++ src/lib/orm/index.ts
@@ -1,4 +1,4 @@
-import { string, uuid } from "./column/index.js";
+import { date, string, uuid } from "./column/index.js";
 import { createOrm } from "./orm/index.js";
 import { defineTable } from "./table/index.js";
 
@@ -10,6 +10,7 @@ const usersTable = defineTable("users", {
   firstName: string("first_name").notNull(),
   lastName: string("last_name").notNull(),
   email: string("email").notNull().unique(),
+  createdAt: date("created_at").notNull().default(() => new Date()),
 });
 
 const orm = createOrm({
@@ -26,19 +27,23 @@ await orm.$raw`
     id TEXT PRIMARY KEY NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE
+    email TEXT NOT NULL UNIQUE,
+    created_at DATETIME NOT NULL
   )
 `;
 console.timeEnd("create table");
 
 console.time("insert users");
+const now = new Date();
+const oneHourAgo = new Date(now.getTime() - 60 * 60 * 1000);
+
 await orm.$raw`
-  INSERT INTO users (id, first_name, last_name, email) VALUES
-  (${Bun.randomUUIDv7()}, 'John', 'Doe', 'john@example.com'),
-  (${Bun.randomUUIDv7()}, 'Jane', 'Smith', 'jane@example.com'),
-  (${Bun.randomUUIDv7()}, 'Bob', 'Johnson', 'bob@example.com'),
-  (${Bun.randomUUIDv7()}, 'Alice', 'Williams', 'alice@example.com'),
-  (${Bun.randomUUIDv7()}, 'Charlie', 'Brown', 'charlie@example.com')
+  INSERT INTO users (id, first_name, last_name, email, created_at) VALUES
+  (${Bun.randomUUIDv7()}, 'John', 'Doe', 'john@example.com', ${oneHourAgo.toISOString()}),
+  (${Bun.randomUUIDv7()}, 'Jane', 'Smith', 'jane@example.com', ${oneHourAgo.toISOString()}),
+  (${Bun.randomUUIDv7()}, 'Bob', 'Johnson', 'bob@example.com', ${now.toISOString()}),
+  (${Bun.randomUUIDv7()}, 'Alice', 'Williams', 'alice@example.com', ${now.toISOString()}),
+  (${Bun.randomUUIDv7()}, 'Charlie', 'Brown', 'charlie@example.com', ${now.toISOString()})
 `;
 console.timeEnd("insert users");
 
@@ -51,6 +56,9 @@ const users = await orm.users.findMany({
     firstName: "John",
     email: {
       contains: "@example.com",
+    },
+    createdAt: {
+      lt: now,
     },
   },
 });
