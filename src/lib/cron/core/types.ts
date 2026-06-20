2c5d514ca631c1d2433fdf002f735cfec978d024
Stefano Labianca <lstefano@proton.me>
2026-06-20 17:19:57 +0200

    removed redundant type

--- src/lib/cron/core/types.ts
+++ src/lib/cron/core/types.ts
@@ -6,10 +6,7 @@ export type ErrorMetadataType = {
   error: Error;
 };
 
-export type ScheduleType =
-  | Bun.CronWithAutocomplete
-  | MinutelyAlias
-  | (string & {});
+export type ScheduleType = Bun.CronWithAutocomplete | MinutelyAlias;
 
 export type OnFailedAttemptContextType = {
   error: Error;
